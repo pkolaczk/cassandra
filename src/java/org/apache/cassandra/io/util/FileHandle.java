0df4e744c4f8525e4e21c8f0beefa9dc2219e368
Daniel Jatnieks <jatnieks@pobox.com>
2026-09-17 14:05:25 -0700

    CNDB-15086: Do not use LimitingRebufferer when limit is actually disabled (#2633)
    ### What is the issue
    There is no need to create the LimitingRebufferer if rate limiting is
    actually disabled.
    
    ### What does this PR fix and why was it fixed
    Do not create the LimitingRebufferer when not needed.
    
    Note: the compaction rate is supposed to be modifiable at runtime. This
    change allows to do so. Because the change applies only to the case we
    open a Rebufferer, for a FileHandle that is a short lived object.
    
    Co-authored-by: Enrico Olivelli <enrico.olivelli@ibm.com>
    Co-authored-by: Zhao Yang <zhaoyangsingapore@gmail.com>

--- src/java/org/apache/cassandra/io/util/FileHandle.java
+++ src/java/org/apache/cassandra/io/util/FileHandle.java
@@ -227,7 +227,7 @@ public class FileHandle extends SharedCloseableImpl
                                 ? PrefetchingRebufferer.withPrefetching(rebuffererFactory)
                                 : rebuffererFactory.instantiateRebufferer(forScan);
 
-        if (limiter != null)
+        if (limiter != null && limiter.getRate() < Double.MAX_VALUE)
             rebufferer = new LimitingRebufferer(rebufferer, limiter, DiskOptimizationStrategy.MAX_BUFFER_SIZE);
         return rebufferer;
     }
